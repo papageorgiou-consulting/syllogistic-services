e7d6ca1caaf92ae6509582272c1889d8fbfd57a9
Adam <adamagne@syllogism.cc>
2024-12-08 03:08:39 +0200

    Show amount per tier.

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,21 +1,41 @@
 import streamlit as st
 
 
-def calculate_percentage(number):
+def find_segments(number):
     if number <= 10000:
-        return (9 * number)/100
+        return {
+            number: (9 * number)/100
+        }
     elif number <= 20000:
-        return 900 + (22 * (number-10000)) / 100
+        return {
+            "0-10000€": 900,
+            f"10001-{number}€": (22 * (number-10000)) / 100
+        }
     elif number <= 30000:
-        return 900 + 2200 + (28 * (number-20000)) / 100
+        return {
+            "0-10000€": 900,
+            "10001-20000€": 2200,
+            f"20001-{number}€": (28 * (number-20000)) / 100
+        }
     elif number <= 40000:
-        return 900 + 2200 + 2800 + (36 * (number-30000)) / 100
+        return {
+            "0-10000€": 900,
+            "10001-20000€": 2200,
+            "20001-30000€": 2800,
+            f"30001-{number}€": (36 * (number-30000)) / 100
+        }
     else:
-        return 900 + 2200 + 2800 + 3600 + (44 * (number-40000)) / 100
+        return {
+            "0-10000€": 900,
+            "10001-20000€": 2200,
+            "20001-30000€": 2800,
+            "30001-40000€": 3600,
+            f"40001-{number}€": (44 * (number-40000)) / 100
+        }
 
 
 def main():
-    st.title("Percentage Calculator")
+    st.title("Tax Calculator")
 
     number = st.number_input(
         "Insert a number", value=None, placeholder="Type a number..."
@@ -24,8 +44,11 @@ def main():
     col1, col2 = st.columns(2)
 
     if number:
-        percentage = calculate_percentage(number)
-        col1.metric("Tax", f"{percentage}€")
+        segments = find_segments(number)
+        col1.write("Calculated tax")
+        col1.metric("Amount", f"{sum(segments.values())}€")
+        col2.write("Tax breakdown")
+        col2.write(segments)
 
 
 if __name__ == "__main__":
